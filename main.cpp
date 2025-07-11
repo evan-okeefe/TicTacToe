dc155cc55ab1df2cfe9d4a9db845aa75092b4797
evan-okeefe <73354669+evan-okeefe@users.noreply.github.com>
2025-07-10 17:08:58 -0700

    Finished

--- main.cpp
+++ main.cpp
@@ -2,6 +2,118 @@
 
 using namespace std;
 
-int main() {
-    cout << "Hello World";
+bool finished = false;
+//true -> player 1
+//false -> player 2
+bool playerTurn = true;
+int board[3][3];
+
+void printBoard() {
+    for (int i = 0; i < 3; i++) {
+        for (int ii = 0; ii < 3; ii++) {
+            if (board[i][ii] == 0) {
+                cout << "[ ]";
+            }
+            else {
+                if (board[i][ii] == 1) {
+                    cout << "[X]";
+                }
+                else {
+                    cout << "[O]";
+                }
+            }
+        }
+        cout << "\n";
+    }
 }
+
+bool markSpot(int row, int col) {
+    if (row >= 3 || col >=3) {
+        return false;
+    }
+    if (board[row][col] == 0) {
+        if (playerTurn) {
+            board[row][col] = 1;
+        }
+        else {
+            board[row][col] = 2;
+        }
+        return true;
+    }
+    return false;
+}
+
+bool checkBoard() {
+    int mark;
+    playerTurn ? mark = 1 : mark = 2;
+    for (int r = 0; r < 3; r++) {
+        if (board[r][0] == mark && board[r][1] == mark && board[r][2] == mark) {
+            return true;
+        }
+    }
+    for (int c = 0; c < 3; c++) {
+        if (board[0][c] == mark && board[1][c] == mark && board[2][c] == mark) {
+            return true;
+        }
+    }
+    if (board[0][0] == mark && board[1][1] == mark && board [2][2] == mark) {
+        return true;
+    }
+    if (board[0][2] == mark && board[1][1] == mark && board [2][0] == mark) {
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    cout << "TIC TAC TOE!\n";
+    cout << "\n";
+    while (!finished) {
+        if (playerTurn) {
+            printBoard();
+            cout << "Player 1 turn";
+            cout << "\n";
+            cout << "Make your selection (Row then Column starting at 0 and going to 2)\n";
+            int row, column;
+
+            cin >> row;
+            cin >> column;
+
+            if (!markSpot(row, column)) {
+                cout << "Invalid selection, please select an empty space in the range of the board \n";
+            }
+
+            if (checkBoard()) {
+                finished = true;
+            }
+            else {
+                playerTurn = !playerTurn;
+            }
+        }
+        else {
+            printBoard();
+            cout << "Player 2 turn";
+            cout << "\n";
+            cout << "Make your selection (Row then Column starting at 0 and going to 2)\n";
+            int row, column;
+
+            cin >> row;
+            cin >> column;
+
+            if (!markSpot(row, column)) {
+                cout << "Invalid selection, please select an empty space in the range of the board \n";
+            }
+
+            if (checkBoard()) {
+                finished = true;
+            }
+            else {
+                playerTurn = !playerTurn;
+            }
+        }
+    }
+
+    playerTurn ? cout << "Player 1 wins" : cout << "Player 2 wins";
+
+}
+
